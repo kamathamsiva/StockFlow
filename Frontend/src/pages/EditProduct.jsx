e0f6a255117ea79121f005b17c1b1b0bb7cd26de
kamathamsiva <kamathamsiva23@gmail.com>
2026-07-11 13:21:56 +0530

    change the editProduct spinner

--- Frontend/src/pages/EditProduct.jsx
+++ Frontend/src/pages/EditProduct.jsx
@@ -104,7 +104,13 @@ function EditProduct() {
       className="d-flex justify-content-center align-items-center"
       style={{ minHeight: "70vh" }}
     >
-      
+      <div
+        className="spinner-border text-primary"
+        role="status"
+        style={{ width: "3rem", height: "3rem" }}
+      >
+        <span className="visually-hidden">Loading...</span>
+      </div>
     </div>
   );
 }
